66bd28054f97b1f6206c9d322bc33877c60839a3
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-10-25 17:13:51 -0400

    mensajes con 'x' y se muestran a despues del boton de agregar

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-document.getElementById("agregar-organizador-btn").addEventListener("click", function() {
-            document.getElementById("modal").style.display = "block";
+document.getElementById("agregar-organizador-btn").addEventListener("click", function () {
+        document.getElementById("modal").style.display = "block";
+    });
+
+    document.getElementById("cerrar-modal-btn").addEventListener("click", function () {
+        document.getElementById("modal").style.display = "none";
+    });
+
+    document.getElementById("organizador-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const nombre = document.getElementById("nombre").value;
+
+        // Crear un nuevo elemento de organizador
+        const nuevoOrganizador = document.createElement("div");
+        nuevoOrganizador.className = "organizador";
+        nuevoOrganizador.textContent = nombre;
+
+        // Crear un botón de borrar
+        const borrarBtn = document.createElement("button");
+        borrarBtn.textContent = "X";
+        borrarBtn.className = "borrar-btn";
+
+        // Agregar un manejador de eventos para el botón de borrar
+        borrarBtn.addEventListener("click", function () {
+            nuevoOrganizador.remove(); // Eliminar el organizador
         });
 
-        document.getElementById("cerrar-modal-btn").addEventListener("click", function() {
-            document.getElementById("modal").style.display = "none";
-        });
+        // Agregar el botón de borrar al organizador
+        nuevoOrganizador.appendChild(borrarBtn);
 
-        document.getElementById("organizador-form").addEventListener("submit", function(event) {
-            event.preventDefault();
+        // Agregar el nuevo organizador al contenedor de organizadores
+        document.getElementById("organizadores-container").appendChild(nuevoOrganizador);
 
-            const nombre = document.getElementById("nombre").value;
-
-            // Crear un nuevo elemento de lista con el nombre del organizador y fondo verde
-            const nuevoOrganizador = document.createElement("li");
-            nuevoOrganizador.textContent = nombre;
-
-            // Agregar el nuevo organizador a la lista horizontal
-            document.getElementById("organizadores-list").appendChild(nuevoOrganizador);
-
-            // Limpiar el formulario y ocultar el modal
-            document.getElementById("nombre").value = "";
-            document.getElementById("modal").style.display = "none";
-        });
+        // Limpiar el formulario y ocultar el modal
+        document.getElementById("nombre").value = "";
+        document.getElementById("modal").style.display = "none";
+    });
